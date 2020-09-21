0e4a1e3e63cb24cf14663daa79a28b0bbd3d09f6
kis90 <69447904+kis90@users.noreply.github.com>
2020-09-21 11:31:26 +0530

    added admin password

--- script/json.js
+++ script/json.js
@@ -2,5 +2,6 @@ var obj = [
   {"EMail": "venkat@gmail.com", "Password": "venkat01"}, 
   {"EMail": "amit@gmail.com", "Password": "amit01"}, 
   {"EMail": "kishan@gmail.com", "Password": "kishan01"}, 
-  {"EMail": "dilip@gmail.com", "Password": "dilip01"}
+  {"EMail": "dilip@gmail.com", "Password": "dilip01"},
+  {"EMail": "admin@gmail.com", "Password": "admin01"}
 ];
