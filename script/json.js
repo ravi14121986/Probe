57e69624268f459a3e376457c5e9c7710eeb9720
kis90 <69447904+kis90@users.noreply.github.com>
2020-09-21 10:29:52 +0530

    Add files via upload

---
+++ script/json.js
@@ -0,0 +1,6 @@
+var obj = [
+  {"EMail": "venkat@gmail.com", "Password": "venkat01"}, 
+  {"EMail": "amit@gmail.com", "Password": "amit01"}, 
+  {"EMail": "kishan@gmail.com", "Password": "kishan01"}, 
+  {"EMail": "dilip@gmail.com", "Password": "dilip01"}
+];
