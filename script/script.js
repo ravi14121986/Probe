37f0f0656b410b2f0b25b322124808536659e3b5
Adithya <adithya.rupanagudi@gmail.com>
2022-06-06 14:15:20 +0530

    Register process

--- script/script.js
+++ script/script.js
@@ -77,6 +77,41 @@ var assessmentTable = $('#example').DataTable({
       }]
    });
 
+   $(document).on("click","#RegisterMemLogin",function() {
+
+		username = document.getElementById("emailLogin").value;
+		password =  document.getElementById("passwordInputRegister").value;
+
+		poolData = {
+				UserPoolId : _config.cognito.userPoolId, // Your user pool id here
+				ClientId : _config.cognito.clientId // Your client id here
+			};
+		var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+		var attributeList = [];
+
+		var dataEmail = {
+			Name : 'email',
+			Value : username, //get from form field
+		};
+
+		var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+
+		attributeList.push(attributeEmail);
+
+		userPool.signUp(username, password, attributeList, null, function(err, result){
+			if (err) {
+				alert(err.message || JSON.stringify(err));
+				return;
+			}
+			cognitoUser = result.user;
+			console.log('user name is ' + cognitoUser.getUsername());
+			//change elements of page
+			document.getElementById("titleheader").innerHTML = "Check your email for a verification link";
+
+		});
+	  }
+ 		);
 
 	$(document).on("click","#submitMemLogin",function() {
 
